579bdc60878240099eafd0fdd71a125997475d94
andilin75 <ilinav75@mail.ru>
2021-02-07 15:47:59 +0300

    added AJAX

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import HttpResponseRedirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 from mainapp.models import Product
 from basket.models import Basket
@@ -26,3 +28,21 @@ def basket_remove(request, id):
     basket = Basket.objects.get(id=id)
     basket.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, id, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        basket = Basket.objects.get(id=int(id))
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+        baskets = Basket.objects.filter(user=request.user)
+        context = {
+            'baskets': baskets,
+        }
+        result = render_to_string('basket/basket.html', context)
+        return JsonResponse({'result': result})
