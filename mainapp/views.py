96d397aa817cb162c5e0234d4072eeb59a8344d6
andilin75 <ilinav75@mail.ru>
2021-02-14 16:59:48 +0300

    added pagination and filtering by category

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 from .models import ProductCategory, Product
 
 # Create your views here.
@@ -11,10 +13,26 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, pk=None):
+# def products(request, category_id=None):
+#     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     context = {
+#         'title': 'geekShop - Каталог',
+#         'categories': ProductCategory.objects.all(),
+#         'products': products,
+#     }
+#     return render(request, 'mainapp/products.html', context)
+
+def products(request, category_id=None, page=1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    per_page = 3
+    paginator = Paginator(products.order_by('price'), per_page)
+    products_paginator = paginator.page(page)
     context = {
         'title': 'geekShop - Каталог',
-        'products': Product.objects.all(),
         'categories': ProductCategory.objects.all(),
+        'products': products_paginator,
     }
     return render(request, 'mainapp/products.html', context)
